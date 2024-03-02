7aff927697f7d822b1193a1205582b35b3bd406b
r0usta <levder@spsejecna.cz>
2024-03-02 16:27:34 +0100

    Add class for loading configuration

---
+++ Peer2P/Lib/Configuration/Configurator.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Peer2P.Lib.Configuration;
+
+public static class Configurator
+{
+    private static string WorkingDirectory { get; } = Directory.GetCurrentDirectory();
+
+    public static string ConfigFileName { get; set; } = "appsettings.json";
+
+    private static string GetValidatedConfigFilePath()
+    {
+        if (string.IsNullOrWhiteSpace(ConfigFileName))
+            throw new ArgumentException("Configuration file name is invalid or not declared", nameof(ConfigFileName));
+
+        return Path.Combine(WorkingDirectory, ConfigFileName);
+    }
+
+    public static IConfigurationBuilder InitBuilder()
+    {
+        string filePath = GetValidatedConfigFilePath();
+
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException(
+                $"The config file '{ConfigFileName}' does not exist at the specified path: {WorkingDirectory}");
+
+        return new ConfigurationBuilder().AddJsonFile(ConfigFileName, true, false);
+    }
+}
