9064d3d4f3315beedcafa0c2ab057f8b2ad8d424
r0usta <levder@spsejecna.cz>
2024-03-02 21:30:21 +0100

    Add static class for network calculations

---
+++ Peer2P/Services/Network/InterfaceCalculator.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Peer2P.Services.Network;
+
+internal static class InterfaceCalculator
+{
+    public static IPAddress GetIpAddress(int interfaceId)
+    {
+        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+        if (interfaceId > host.AddressList.Length)
+            throw new ArgumentOutOfRangeException(nameof(interfaceId));
+
+        return host.AddressList[interfaceId - 1];
+    }
+    
+    public static IPAddress GetSubnetMask(IPAddress ipAddress)
+    {
+        NetworkInterface? networkInterface = NetworkInterface
+            .GetAllNetworkInterfaces()
+            .FirstOrDefault(i =>
+                i.GetIPProperties().UnicastAddresses.Any(addr => Equals(addr.Address, ipAddress)));
+
+        return networkInterface?.GetIPProperties().UnicastAddresses
+            .FirstOrDefault(addr => Equals(addr.Address, ipAddress))?.IPv4Mask ?? IPAddress.None;
+    }
+    
+    public static IPAddress GetBroadcast(IPAddress ipAddress, IPAddress subnetMask)
+    {
+        byte[] ipAddressBytes = ipAddress.GetAddressBytes();
+        byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+        if (ipAddressBytes.Length != subnetMaskBytes.Length)
+            throw new ArgumentException($"Lengths of IP address [{ipAddress}] and subnet mask [{subnetMask}] do not match.");
+
+        byte[] broadcastAddress = new byte[ipAddressBytes.Length];
+        for (int i = 0; i < broadcastAddress.Length; i++)
+            broadcastAddress[i] = (byte)(ipAddressBytes[i] | (subnetMaskBytes[i] ^ 255));
+        return new IPAddress(broadcastAddress);
+    }
+}
