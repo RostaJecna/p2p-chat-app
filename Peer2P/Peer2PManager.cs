b2e31c0e3f6645dfe269e321b568b6965d5e28de
r0usta <levder@spsejecna.cz>
2024-03-02 21:32:45 +0100

    Add handling configuration loads

--- Peer2P/Peer2PManager.cs
+++ Peer2P/Peer2PManager.cs
@@ -1,9 +1,24 @@
+using Peer2P.Library.Console.Messaging;
+using Peer2P.Services;
+
 namespace Peer2P;
 
 public static class Peer2PManager
 {
     public static bool TryInitialize()
     {
+        if (!SettingsLoader.TryLoadFromSection("Peer2P") || !SettingsLoader.TrySetupNetInterface())
+        {
+            Logger.Log("Failed to load settings for the Peer2P library.")
+                .Type(LogType.Error).Display();
+            return false;
+        }
+        
+        Logger.Log("The settings were successfully loaded for the Peer2P library.")
+            .Type(LogType.Successful).Display();
+        
+        
+        
         return true;
     }
 }
