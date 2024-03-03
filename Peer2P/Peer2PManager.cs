2f44595e6fdc9f5f185bd4cde57554cc2189d9d1
r0usta <levder@spsejecna.cz>
2024-03-03 17:45:12 +0100

    Start udp discovery methods, add cancellation token

--- Peer2P/Peer2PManager.cs
+++ Peer2P/Peer2PManager.cs
@@ -1,11 +1,13 @@
+using Peer2P.Library.Connection;
 using Peer2P.Library.Console.Messaging;
 using Peer2P.Services;
+using Peer2P.Services.Connection;
 
 namespace Peer2P;
 
 public static class Peer2PManager
 {
-    public static bool TryInitialize()
+    public static bool TryInitialize(CancellationToken cancellationToken)
     {
         if (!SettingsLoader.TryLoadFromSection("Peer2P") || !SettingsLoader.TrySetupNetInterface())
         {
@@ -14,6 +16,19 @@ public static class Peer2PManager
             return false;
         }
         
-        return true;
+        try
+        {
+            UdpDiscovery.SendPeriodicAsync(NetMessages.ReqResPair.Command, cancellationToken);
+            UdpDiscovery.ListenIncomingAsync(cancellationToken);
+            
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Logger.Log($"An unexpected error occurred while running services in the Peer2P library: {ex.Message}")
+                .Type(LogType.Error).Display();
+            UdpDiscovery.Dispose();
+            return false;
+        }
     }
 }
