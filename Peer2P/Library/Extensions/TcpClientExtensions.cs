e65538c850c9b362f2c9e332dcd56eca2e4caf3d
r0usta <levder@spsejecna.cz>
2024-03-07 14:14:23 +0100

    Add tcp client extension class for helpful methods

---
+++ Peer2P/Library/Extensions/TcpClientExtensions.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using System.Net.Sockets;
+
+namespace Peer2P.Library.Extensions;
+
+public static class TcpClientExtensions
+{
+    public static IPEndPoint? GetIpv4EndPoint(this TcpClient? client)
+    {
+        IPEndPoint? remoteIpEndPoint = (IPEndPoint?)client?.Client.RemoteEndPoint;
+        return remoteIpEndPoint is not null
+            ? new IPEndPoint(remoteIpEndPoint.Address.MapToIPv4(), remoteIpEndPoint.Port)
+            : null;
+    }
+    
+    public static bool IsSameIpv4Address(this TcpClient? tcpClient, IPAddress ipAddress)
+    {
+        IPEndPoint? remoteIpEndPoint = tcpClient.GetIpv4EndPoint();
+        return remoteIpEndPoint is not null && remoteIpEndPoint.Address.Equals(ipAddress);
+    }
+    
+    public static bool IsStillConnected(this TcpClient? tcpClient)
+    {
+        return tcpClient is { Connected: true };
+    }
+}
