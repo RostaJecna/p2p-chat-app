8ede8afeb7d34ebe5c74679f640e25e96bcc2e97
r0usta <levder@spsejecna.cz>
2024-03-11 19:10:52 +0100

    Reformat class

--- Peer2P/Services/SettingsLoader.cs
+++ Peer2P/Services/SettingsLoader.cs
@@ -9,12 +9,12 @@ using Peer2P.Services.Connection;
 namespace Peer2P.Services;
 
 /// <summary>
-/// Provides functionality for loading and setting up various settings for the Peer2P library.
+///     Provides functionality for loading and setting up various settings for the Peer2P library.
 /// </summary>
 internal static class SettingsLoader
 {
     /// <summary>
-    /// Attempts to load settings from the specified configuration section.
+    ///     Attempts to load settings from the specified configuration section.
     /// </summary>
     /// <param name="sectionName">The name of the configuration section to load settings from.</param>
     /// <returns><c>true</c> if settings are successfully loaded; otherwise, <c>false</c>.</returns>
@@ -56,31 +56,33 @@ internal static class SettingsLoader
     }
 
     /// <summary>
-    /// Attempts to set up the network interface based on the configured interface ID.
+    ///     Attempts to set up the network interface based on the configured interface ID.
     /// </summary>
     /// <returns><c>true</c> if the network interface is successfully set up; otherwise, <c>false</c>.</returns>
     public static bool TrySetupNetInterface()
     {
         int interfaceId = Peer2PSettings.Instance.Global.NetworkInterfaceId;
-        
+
         try
         {
             int broadcastPort = Peer2PSettings.Instance.Communication.BroadcastPort;
-            
+
             IPAddress ipAddress = InterfaceCalculator.GetIpAddress(interfaceId);
             IPAddress subnetMask = InterfaceCalculator.GetSubnetMask(ipAddress);
             IPAddress broadcastAddress = InterfaceCalculator.GetBroadcast(ipAddress, subnetMask);
             IPEndPoint broadcastEndPoint = new(broadcastAddress, broadcastPort);
-            
+
             Peer2PSettings.Instance.Network = new NetworkSettings(ipAddress, subnetMask, broadcastEndPoint);
             return true;
         }
         catch (Exception exception)
         {
-            Logger.Log($"An unexpected error occurred while setting up the network interface. Interface ID: [{interfaceId}]")
+            Logger.Log(
+                    $"An unexpected error occurred while setting up the network interface. Interface ID: [{interfaceId}]")
                 .Type(LogType.Error).Display();
             Logger.Log($"Error message: {exception.Message}").Type(LogType.Error).Display();
-            Logger.Log($"Check if the interface specified in the '{Configurator.ConfigFileName}' config meets the conditions!")
+            Logger.Log(
+                    $"Check if the interface specified in the '{Configurator.ConfigFileName}' config meets the conditions!")
                 .Type(LogType.Warning).Display();
             return false;
         }
